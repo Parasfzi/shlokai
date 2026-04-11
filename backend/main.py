6ebbd4037f9c96e6f459c623b3f2a482d377dedc
Paras Pawar <pawarparas855@gmail.com>
2026-04-12 01:39:51 +0530

    feat: initialize backend main entry point for gitaGuruAi

--- backend/main.py
+++ backend/main.py
@@ -71,7 +71,7 @@ app.add_middleware(
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
         "http://192.168.1.2:5173", # LAN Local testing
-        "https://shlokai.vercel.app"
+        "https://shlokai.paraspawar.in/"
     ], 
     allow_credentials=True,
     allow_methods=["*"],
